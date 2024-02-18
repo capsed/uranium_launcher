0412b754d28305079995127c07f8e8a895be5809
capsed <abderrahmensarraj@gmail.com>
2024-02-17 19:25:50 -0500

    new minecraft 1.8.9 client

---
+++ build.sh
@@ -0,0 +1,3 @@
+cd launcher
+javac Main.java
+cd -
---
+++ launcher/Main.java
@@ -0,0 +1,14 @@
+class Main{
+public static void main(String[] args) {
+   
+    System.out.println("starting uranium client");
+    
+}
+}
+
+class launch {
+    public void launching(){
+        
+    }
+   
+}
